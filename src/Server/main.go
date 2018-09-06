a9bf3080504a3af9254c29d09bfe694750fad8fb
OctopusLian <zoctopus@qq.com>
2018-09-06 12:41:29 +0800

    run server success

--- src/Server/main.go
+++ src/Server/main.go
@@ -1,2 +1,27 @@
 package main
 
+import (
+	"net"
+	"fmt"
+)
+
+func main(){
+	listen_socket, err := net.Listen("tcp", "127.0.0.1:8000")  //打开监听接口
+	if err != nil { //如果有错误
+		fmt.Println("sever error")
+	}
+
+	defer listen_socket.Close()  //延迟服务器端关闭
+	fmt.Println("sever is wating ....")
+
+
+	for {
+		conn, err := listen_socket.Accept() //监听客户端的端口
+		if err != nil {
+			fmt.Println("conn fail ...")
+		}
+		fmt.Println("connect client successed") //显示服务器端连接成功
+	}
+		fmt.Println("client Close\n")
+		conn.Close()
+}
--- src/Tools/format.go
+++ src/Tools/format.go
@@ -1,1 +1,22 @@
 package Tools
+
+func min(a int,b int)int{
+	if(a < b){
+		return a
+	}else{
+		return b
+	}
+}
+
+func max(a int,b int)int{
+	if(a > b){
+		return a
+	}else{
+		return b
+	}
+}
+
+func new(x,y)int{
+	return {x=x,y=y}
+}
+
